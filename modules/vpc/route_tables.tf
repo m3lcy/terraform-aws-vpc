638b7a80f73f2d649387c58a1d3445e0f2a5bcb3
root <shrilly3110@gmail.com>
2026-03-13 19:03:07 -0400

    update route table to create private routetable per private subnet

--- modules/vpc/route_tables.tf
+++ modules/vpc/route_tables.tf
@@ -12,18 +12,16 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.this.id
+  for_each = { for k, s in aws_subnet.enterprise_subnets : k => s if !s.map_public_ip_on_launch }
+  vpc_id   = aws_vpc.this.id
 
-  dynamic "route" {
-    for_each = var.enable_nat_gateway ? [1] : []
-    content {
-      cidr_block     = "0.0.0.0/0"
-      nat_gateway_id = aws_nat_gateway.this[0].id
-    }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[each.value.availability_zone].id
   }
 
   tags = {
-    Name = "${var.name_prefix}-private-rt"
+    Name = "${var.name_prefix}-private-rt-${each.key}"
   }
 }
 
@@ -31,5 +29,5 @@ resource "aws_route_table_association" "enterprise_assoc" {
   for_each  = aws_subnet.enterprise_subnets
   subnet_id = each.value.id
 
-  route_table_id = var.subnet_config[each.key].is_public ? aws_route_table.public.id : aws_route_table.private.id
+  route_table_id = var.subnet_config[each.key].is_public ? aws_route_table.public.id : aws_route_table.private[each.key].id
 }
