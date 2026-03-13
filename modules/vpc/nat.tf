03b1931e352046a5187a40f57dd7bae13624d5dc
root <shrilly3110@gmail.com>
2026-03-13 19:01:23 -0400

    update nat.tf to add EIP and NAT Gateway per AZ

--- modules/vpc/nat.tf
+++ modules/vpc/nat.tf
@@ -1,10 +1,15 @@
 locals {
-  public_subnet_ids = [for s in aws_subnet.enterprise_subnets : s.id if s.map_public_ip_on_launch]
+  public_subnets = {
+    for k, s in aws_subnet.enterprise_subnets : k => s if s.map_public_ip_on_launch
+  }
+  public_subnet_by_az = {
+    for az, id in { for s in local.public_subnets : s.availability_zone => s.id } : az => id
+  }
 }
 
 resource "aws_eip" "nat" {
-  count  = var.enable_nat_gateway ? 1 : 0
-  domain = "vpc"
+  for_each = var.enable_nat_gateway ? local.public_subnet_by_az : {}
+  domain   = "vpc"
 
   tags = {
     Name = "${var.name_prefix}-nat-eip"
@@ -12,9 +17,9 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "this" {
-  count         = var.enable_nat_gateway ? 1 : 0
-  allocation_id = aws_eip.nat[0].id
-  subnet_id     = local.public_subnet_ids[0]
+  for_each      = var.enable_nat_gateway ? local.public_subnet_by_az : {}
+  allocation_id = aws_eip.nat[each.key].id
+  subnet_id     = each.value
 
   tags = {
     Name = "${var.name_prefix}-nat-gw"
