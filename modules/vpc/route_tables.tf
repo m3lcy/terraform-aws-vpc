ffbcbe1b25a0f4460482722f7dc8c915be1e0263
root <shrilly3110@gmail.com>
2026-03-13 19:41:20 -0400

    fixed invalid index when nat gateway is set to false

--- modules/vpc/route_tables.tf
+++ modules/vpc/route_tables.tf
@@ -15,9 +15,12 @@ resource "aws_route_table" "private" {
   for_each = { for k, s in aws_subnet.enterprise_subnets : k => s if !s.map_public_ip_on_launch }
   vpc_id   = aws_vpc.this.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this[each.value.availability_zone].id
+  dynamic "route" {
+    for_each = var.enable_nat_gateway ? [1] : []
+    content {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.this[each.value.availability_zone].id
+    }
   }
 
   tags = {
