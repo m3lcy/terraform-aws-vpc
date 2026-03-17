644050509152e01aedbf87f5334bd773640409fb
root <shrilly3110@gmail.com>
2026-03-17 19:08:54 -0400

    add initial VPC & IGW with subnets for prod environment layout

---
+++ environments/prod/main.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr    = var.vpc_cidr
+  name_prefix = var.name_prefix
+
+  subnet_config = var.subnet_config
+
+  management_ssh_cidrs = var.management_ssh_cidrs
+
+  enable_nat_gateway = false
+}
