37d4970d6fbfae64cd6de600b3150197ea5fe2f4
root <shrilly3110@gmail.com>
2026-03-18 17:06:52 -0400

    update prod providers.tf

--- environments/prod/providers.tf
+++ environments/prod/providers.tf
@@ -1,10 +1,6 @@
 provider "aws" {
   region = var.aws_region
 
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_region_validation      = true
-
   default_tags {
     tags = {
       Project     = "Cloud-Enterprise-Network"
