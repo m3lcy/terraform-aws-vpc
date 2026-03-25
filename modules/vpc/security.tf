c546c5a25e9553dcb6077bc12b5a7dc19458e815
root <shrilly3110@gmail.com>
2026-03-25 18:28:16 -0400

    add ingress rules for security groups

--- modules/vpc/security.tf
+++ modules/vpc/security.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "management" {
-  name        = "${var.name_prefix}-mgmt-sg"
+  name        = "${var.environment}-${var.name_prefix}-mgmt-sg"
   description = "Management Services"
   vpc_id      = aws_vpc.this.id
 
@@ -9,7 +9,7 @@ resource "aws_security_group" "management" {
 }
 
 resource "aws_security_group" "compute" {
-  name        = "${var.name_prefix}-compute-sg"
+  name        = "${var.environment}-${var.name_prefix}-compute-sg"
   description = "High-Bandwidth Workloads"
   vpc_id      = aws_vpc.this.id
 
@@ -19,7 +19,7 @@ resource "aws_security_group" "compute" {
 }
 
 resource "aws_security_group" "enterprise_internal" {
-  name        = "${var.name_prefix}-internal-sg"
+  name        = "${var.environment}-${var.name_prefix}-internal-sg"
   description = "Core, R&D, and Employee Services"
   vpc_id      = aws_vpc.this.id
 
@@ -29,7 +29,7 @@ resource "aws_security_group" "enterprise_internal" {
 }
 
 resource "aws_security_group" "guest" {
-  name        = "${var.name_prefix}-guest-sg"
+  name        = "${var.environment}-${var.name_prefix}-guest-sg"
   description = "Guest Traffic"
   vpc_id      = aws_vpc.this.id
 
@@ -46,6 +46,10 @@ resource "aws_vpc_security_group_ingress_rule" "mgmt_ssh" {
   to_port           = 22
   ip_protocol       = "tcp"
   cidr_ipv4         = each.value
+
+  tags = {
+    Name = "mgmt-ssh"
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "mgmt_to_compute" {
@@ -54,6 +58,66 @@ resource "aws_vpc_security_group_ingress_rule" "mgmt_to_compute" {
   to_port                      = 22
   ip_protocol                  = "tcp"
   referenced_security_group_id = aws_security_group.management.id
+
+  tags = {
+    Name = "mgmt-to-compute"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "compute_from_internal_https" {
+  security_group_id            = aws_security_group.compute.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.enterprise_internal.id
+
+  tags = {
+    Name = "compute-from-internal-https"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "compute_from_internal_app" {
+  security_group_id            = aws_security_group.compute.id
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.enterprise_internal.id
+
+  tags = {
+    Name = "compute-from-internal-app"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "internal_from_mgmt_ssh" {
+  security_group_id            = aws_security_group.enterprise_internal.id
+  from_port                    = 22
+  to_port                      = 22
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.management.id
+
+  tags = {
+    Name = "internal-from-mgmt-ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "guest_http" {
+  security_group_id = aws_security_group.guest.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+
+  tags = { Name = "guest-inbound-http" }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "guest_https" {
+  security_group_id = aws_security_group.guest.id
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+
+  tags = { Name = "guest-inbound-https" }
 }
 
 resource "aws_vpc_security_group_egress_rule" "all_outbound" {
