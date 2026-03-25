de43d7d52e91f9d2a8f5d056f4a341113fce20c5
root <shrilly3110@gmail.com>
2026-03-25 18:02:00 -0400

    add providers

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,9 +1,16 @@
+terraform {
+  required_version = "~> 1.5.0" 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0" 
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
-
-  # skip_credentials_validation = true
-  # skip_requesting_account_id  = true
-  # skip_region_validation      = true
 
   default_tags {
     tags = {
