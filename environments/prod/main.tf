3a9d6d2708a800b3d7baabcb93c129c2c1207f1e
root <shrilly3110@gmail.com>
2026-04-01 18:51:44 -0400

    add flow log vars to prod main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -5,9 +5,10 @@ module "vpc" {
   vpc_cidr    = var.vpc_cidr
   name_prefix = var.name_prefix
 
-  subnet_config = var.subnet_config
-
+  subnet_config        = var.subnet_config
   management_ssh_cidrs = var.management_ssh_cidrs
+  enable_nat_gateway   = true
 
-  enable_nat_gateway = false
+  enable_flow_logs      = var.enable_flow_logs
+  flow_log_traffic_type = var.flow_log_traffic_type
 }
