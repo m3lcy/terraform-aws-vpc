19133a9b8b5ee5f073f1e9037d90c1ad26cedbfe
root <shrilly3110@gmail.com>
2026-04-17 16:37:39 -0400

    add shared-svc main.tf with remote state data sources

---
+++ environments/shared-svc/main.tf
@@ -0,0 +1,44 @@
+data "terraform_remote_state" "tgw" {
+  backend = "s3"
+  config = {
+    bucket = "m3lcy-terraform-state"
+    key    = "cloud-enterprise-network/tgw/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "prod" {
+  backend = "s3"
+  config = {
+    bucket = "m3lcy-terraform-state"
+    key    = "cloud-enterprise-network/prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+locals {
+  remote_tgw_routes = try(
+    [
+      {
+        destination_cidr = data.terraform_remote_state.prod.outputs.vpc_cidr
+        tgw_id           = data.terraform_remote_state.tgw.outputs.transit_gateway_id
+      }
+    ],
+    []
+  )
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  environment           = var.environment
+  vpc_cidr              = var.vpc_cidr
+  name_prefix           = var.name_prefix
+  subnet_config         = var.subnet_config
+  management_ssh_cidrs  = var.management_ssh_cidrs
+  enable_nat_gateway    = false
+  enable_flow_logs      = var.enable_flow_logs
+  flow_log_traffic_type = var.flow_log_traffic_type
+
+  tgw_routes = length(var.tgw_routes) > 0 ? var.tgw_routes : local.remote_tgw_routes
+}
