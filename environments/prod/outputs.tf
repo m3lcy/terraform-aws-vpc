e63f5193205ef8bbeb96295c1fadb8a073d8c854
root <shrilly3110@gmail.com>
2026-04-17 16:18:00 -0400

    add prod env outputs.tf

---
+++ environments/prod/outputs.tf
@@ -0,0 +1,28 @@
+output "vpc_id" {
+  description = "Shared services VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "subnet_ids_by_key" {
+  description = "Subnet IDs keyed by subnet name"
+  value       = module.vpc.subnet_ids_by_key
+}
+
+output "vpc_cidr" {
+  description = "Prod VPC CIDR"
+  value       = module.vpc.vpc_cidr
+}
+
+output "mgmt_subnet_ids" {
+  description = "Prod mgmt subnet IDs for TGW attachment"
+  value = [
+    module.vpc.subnet_ids_by_key["mgmt-1a"],
+    module.vpc.subnet_ids_by_key["mgmt-1b"],
+    module.vpc.subnet_ids_by_key["mgmt-1c"],
+  ]
+}
