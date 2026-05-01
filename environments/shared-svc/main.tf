0f77dac962f2db45f552ebd3d4d20f7ae2f8d46b
root <shrilly3110@gmail.com>
2026-04-30 23:04:16 -0400

    add ec2 instance module to prod

--- environments/shared-svc/main.tf
+++ environments/shared-svc/main.tf
@@ -36,9 +36,26 @@ module "vpc" {
   name_prefix           = var.name_prefix
   subnet_config         = var.subnet_config
   management_ssh_cidrs  = var.management_ssh_cidrs
-  enable_nat_gateway    = false
+  enable_nat_gateway    = true
   enable_flow_logs      = var.enable_flow_logs
   flow_log_traffic_type = var.flow_log_traffic_type
 
   tgw_routes = length(var.tgw_routes) > 0 ? var.tgw_routes : local.remote_tgw_routes
+
+  trusted_cidrs = [data.terraform_remote_state.prod.outputs.vpc_cidr ]
+}
+
+module "ec2_instance" {
+  source = "../../modules/ec2"
+
+  environment = var.environment
+  name_prefix = var.name_prefix
+
+  instances = {
+    bastion = {
+      subnet_id         = module.vpc.subnet_ids_by_key["mgmt-1a"]
+      security_group_ids = [module.vpc.security_group_ids["management"]]
+      instance_type     = "t3.micro"
+    }
+  }
 }
